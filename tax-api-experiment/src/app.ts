b3b81ed207afb0abcd4ff96a50ebb35173108053
daichi0918 <kayasugadaichi@icloud.com>
2023-04-22 21:34:34 +0900

    fin:5.4

---
+++ tax-api-experiment/src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express'
+const app = express()
+app.use(express.json())
+type CalcInput = {
+  yearsOfService: number
+  isDisability: boolean
+  isOfficer: boolean
+  severancePay: number
+}
+const calcTax = (input: CalcInput) => {
+  // TODO 退職金の所得税を計算する
+  return 10000
+}
+app.post('/calc-tax', (req, res) => {
+  res.json({ tax: calcTax(req.body) })
+})
+export default app
